8dd089115757d478a5e0123f93d3e3362120e367
himoto <himoto@protein.osaka-u.ac.jp>
2019-06-27 12:54:58 +0900

    Function caching

--- fibonacci.py
+++ fibonacci.py
@@ -12,3 +12,15 @@ start = time.time()
 fib(40)
 elapsed_time = time.time() - start
 '''
+
+''' elapled_time = 6.6e-5 [esc.]
+
+from functools import lru_cache
+@lru_cache(maxsize=None)
+def fib_cache(n):
+    if n < 2:
+        return n
+    else:
+        return fib_cache(n-2) + fib_cache(n-1)
+        
+'''
